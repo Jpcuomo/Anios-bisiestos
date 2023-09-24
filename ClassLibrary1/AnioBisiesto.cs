4fc6a8df54b8fc836afb109e9cdf01ca31400135
Jpcuomo <jpcuomo2000@gmail.com>
2023-09-24 16:36:03 -0300

    Agregar archivos de proyecto.

---
+++ ClassLibrary1/AnioBisiesto.cs
@@ -0,0 +1,50 @@
+﻿namespace ClassLibrary1
+{
+    public class AnioBisiesto
+    {
+        private static int _anioInicio { get; set; }
+        private static int _anioFin { get; set; }
+
+
+        /// <summary>
+        /// Informa qué años en el rango ingresado son bisiestos
+        /// </summary>
+        /// <param name="inicio">Año de inicio</param>
+        /// <param name="fin">Año de finalización</param>
+        public static void CalcularAnioBisiesto(int inicio, int fin)
+        {
+            for (int i = inicio; i <= fin; i++) 
+            { 
+                if(i % 4 == 0 && (i % 100 != 0 || i % 400 == 0))
+                {
+                    Console.WriteLine($"{i} es bisiesto");
+                }
+                //Console.WriteLine("No es bisiesto");
+            }
+        }
+
+        /// <summary>
+        /// Pide al usuario un año y valida que sea numérico
+        /// </summary>
+        /// <returns>Entero</returns>
+        public static int IngresarAnio()
+        {
+            int anio;
+            while (true)
+            {
+                Console.WriteLine("Ingrese un año: ");
+                if (int.TryParse(Console.ReadLine(), out anio))
+                {
+                    if (anio > 0)
+                    {
+                        return anio;
+                    }
+                   
+                    Console.WriteLine("El año debe ser mayor que cero.");
+                }
+                Console.WriteLine("Valor incorrecto. Introduzca un año válido.");
+            }
+        }
+
+    }
+}
---
+++ Consola/Program.cs
@@ -0,0 +1,26 @@
+﻿using ClassLibrary1;
+
+namespace Consola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int inicio = AnioBisiesto.IngresarAnio();
+            int final = AnioBisiesto.IngresarAnio();
+            while (true)
+            {
+                if (inicio < final)
+                {
+                    AnioBisiesto.CalcularAnioBisiesto(inicio, final);
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("En año de inico debe ser menor al de finalización");
+                    break;
+                }
+            }
+        }
+    }
+}
